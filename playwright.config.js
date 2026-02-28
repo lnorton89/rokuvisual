a2698ed6e1c9d2d65a68fbc54fda89c4c03e0f71
lnorton89 <lnorton89@gmail.com>
2026-02-27 20:50:24 -0600

    test: Fix flaky tests and improve reliability

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,10 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
     baseURL: 'http://localhost:30002',
